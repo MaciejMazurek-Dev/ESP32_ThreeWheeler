905862801133bef2bdd070b9962643c2dda2bef3
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2026-01-29 14:57:15 +0100

    Add wifi configuration phase

--- wifi/include/wifi_station.h
+++ wifi/include/wifi_station.h
@@ -2,7 +2,13 @@
 #ifndef WIFI_STATION_H
 #define WIFI_STATION_H
 
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+
 
 void wifi_station_init(void);
+void wifi_configure(void);
 
 #endif
--- wifi/wifi_station.c
+++ wifi/wifi_station.c
@@ -1,45 +1,76 @@
 #include "wifi_station.h"
-#include "esp_netif.h"
-#include "esp_log.h"
-#include "esp_event.h"
-#include "esp_wifi.h"
+
+#define WIFI_SSID       CONFIG_WIFI_SSDI
+#define WIFI_PASSWORD   CONFIG_WIFI_PASSWORD
+
 
 static const char TAG[] = "wifi_station"; 
+
+esp_err_t result_err;
+esp_netif_t* result_netif;
 
 
 void wifi_station_init(void)
 {
-    esp_err_t resultErr;
-    esp_netif_t* resultNetif;
-
+    //INIT PHASE
     // Initialize network stack
-    resultErr = esp_netif_init();
-    if(resultErr != ESP_OK)
+    result_err = esp_netif_init();
+    if(result_err != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to initialize TCP/IP stack.");
         return;
     }
 
     // Start default event loop
-    resultErr = esp_event_loop_create_default();
-    if(resultErr != ESP_OK)
+    result_err = esp_event_loop_create_default();
+    if(result_err != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to start event loop.");
         return;
     }
 
     // Create default network interface 
-    resultNetif = esp_netif_create_default_wifi_sta();
-    if(resultNetif != ESP_OK)
+    result_netif = esp_netif_create_default_wifi_sta();
+    if(result_netif != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to create network interface.");
     }
 
-    // Create WiFi driver task and initialize WiFi driver.
+    // Create task and initialize WiFi driver.
     wifi_init_config_t defaultConfiguration = WIFI_INIT_CONFIG_DEFAULT();
-    resultErr = esp_wifi_init(&defaultConfiguration);
-    if(resultErr != ESP_OK)
+    result_err = esp_wifi_init(&defaultConfiguration);
+    if(result_err != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to initialize WiFi driver.");
     }
+
+
+
+    // CONFIGURATION PHASE
+    // Configuration data
+    wifi_config_t wifi_config = {
+                                 .sta = {
+                                            .ssid = WIFI_SSID,
+                                            .password = WIFI_PASSWORD,
+                                            // Set minimum authentication mode
+                                            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+                                        },
+                                };
+
+    // Set to Station Mode                                       
+    result_err = esp_wifi_set_mode(WIFI_MODE_STA);
+    if(result_err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to set WiFi mode.");
+    }
+    
+    result_err = esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    if(result_err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to configure WiFi.");
+    }
+
+
+
+
 }
