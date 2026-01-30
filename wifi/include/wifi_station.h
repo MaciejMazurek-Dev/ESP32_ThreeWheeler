fc52e986ec4795cbdf82c3fc288021541e20a92d
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2026-01-30 23:29:07 +0100

    Add wifi event handler

--- wifi/include/wifi_station.h
+++ wifi/include/wifi_station.h
@@ -7,6 +7,10 @@
 #include "esp_event.h"
 #include "esp_log.h"
 
+static void wifi_event_handler(void* arg, 
+                               esp_event_base_t event_base,
+                               int32_t event_id,
+                               void* event_data);
 
 void wifi_station_init(void);
 void wifi_configure(void);
--- wifi/wifi_station.c
+++ wifi/wifi_station.c
@@ -10,10 +10,21 @@ esp_err_t result_err;
 esp_netif_t* result_netif;
 
 
+static void wifi_event_handler(void* handler_arg, 
+                               esp_event_base_t event_base,
+                               int32_t event_id,
+                               void* event_data)
+{
+    if(event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        result_err = esp_wifi_connect();
+    }
+}
+
+
+
 void wifi_station_init(void)
 {
-    //INIT PHASE
-    // Initialize network stack
     result_err = esp_netif_init();
     if(result_err != ESP_OK)
     {
@@ -21,7 +32,6 @@ void wifi_station_init(void)
         return;
     }
 
-    // Start default event loop
     result_err = esp_event_loop_create_default();
     if(result_err != ESP_OK)
     {
@@ -29,25 +39,22 @@ void wifi_station_init(void)
         return;
     }
 
-    // Create default network interface 
     result_netif = esp_netif_create_default_wifi_sta();
     if(result_netif != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to create network interface.");
+        return;
     }
 
-    // Create task and initialize WiFi driver.
     wifi_init_config_t defaultConfiguration = WIFI_INIT_CONFIG_DEFAULT();
     result_err = esp_wifi_init(&defaultConfiguration);
     if(result_err != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to initialize WiFi driver.");
+        return;
     }
 
 
-
-    // CONFIGURATION PHASE
-    // Configuration data
     wifi_config_t wifi_config = {
                                  .sta = {
                                             .ssid = WIFI_SSID,
@@ -57,18 +64,31 @@ void wifi_station_init(void)
                                         },
                                 };
 
-    // Set to Station Mode                                       
     result_err = esp_wifi_set_mode(WIFI_MODE_STA);
     if(result_err != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to set WiFi mode.");
+        return;
     }
     
     result_err = esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
     if(result_err != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to configure WiFi.");
+        return;
     }
+
+    esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL);
+    esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, NULL);
+
+    // The startup function is asynchronous: it returns immediately and does not wait for the completion of the startup process.
+    result_err = esp_wifi_start();
+    if(result_err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to start WiFi driver.");
+        return;
+    }
+
 
 
 
