b3b7a1ed5a5feacb8e04fc0f4066f6adef03d094
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2026-02-03 17:21:25 +0100

    Add automatic reconnect on WiFi disconnect

--- wifi/wifi_station.c
+++ wifi/wifi_station.c
@@ -22,6 +22,10 @@ static void wifi_start_event_handler(void* handler_arg,
     {
         ESP_LOGI(TAG, "Trying to connect %s access point...", WIFI_SSID);
         result_wifi_err = esp_wifi_connect();
+    }else if (event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        ESP_LOGI(TAG, "Connection failed. Trying again...");
+        esp_wifi_connect();
     }
 }
 
@@ -33,6 +37,7 @@ static void ip_event_handler(void* handler_arg,
 {
     if(event_id == IP_EVENT_STA_GOT_IP)
     {
+        ESP_LOGI(TAG, "Connected with access point. IP");
         xEventGroupSetBits(wifi_event_group_handle, WIFI_CONNECTED_BIT);
     }
 }
@@ -64,7 +69,7 @@ void wifi_station_init(void)
     esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_start_event_handler, NULL, NULL);
     esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler, NULL, NULL);
 
-    ESP_LOGI(TAG, "Initializing WiFi: allocate resources for the driver and start the WiFi task.");   
+    ESP_LOGI(TAG, "Initializing WiFi - Allocate resources for the driver and start the WiFi task.");   
     wifi_init_config_t defaultConfiguration = WIFI_INIT_CONFIG_DEFAULT();
     result_wifi_err = esp_wifi_init(&defaultConfiguration);
     if(result_wifi_err != ESP_OK)
@@ -97,7 +102,7 @@ void wifi_station_init(void)
         return;
     }
 
-    // The startup function is asynchronous: it returns immediately and does not wait for the completion of the startup process.
+    // The startup function is asynchronous, it returns immediately.
     ESP_LOGI(TAG, "Starting WiFi driver.");
     result_wifi_err = esp_wifi_start();
     if(result_wifi_err != ESP_OK)
@@ -115,11 +120,12 @@ void wifi_station_init(void)
         return;
     }
 
-    // Block Task - Wait for connection
-    ESP_LOGI(TAG, "Waiting for connection with access point...");
+    // Block Task - Wait for successful connection
     EventBits_t bits = xEventGroupWaitBits(wifi_event_group_handle, WIFI_CONNECTED_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
 
-    ESP_LOGI(TAG, "Connected to access point.");
+    // TODO: How to handle unsuccessful connection?
+
+    ESP_LOGI(TAG, "Connected with %s access point.", WIFI_SSID);
 
 
 
