e9ff33126a782460473018f9e90b1cd1b1c3806a
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2026-02-16 22:52:12 +0100

    Add basic gpio configuration

---
+++ three_wheeler/include/three_wheeler.h
@@ -0,0 +1,10 @@
+#ifndef THREE_WHEELER_H
+#define THREE_WHEELER_H
+
+#include "driver/gpio.h"
+
+void left_wheel(char speed);
+void right_wheel(char speed);
+
+
+#endif
--- websocket/include/websocket.h
+++ websocket/include/websocket.h
@@ -4,6 +4,8 @@
 #include "esp_http_server.h"
 #include "esp_log.h"
 
+static esp_err_t test_uri_handler(httpd_req_t *req);
+static void register_uri_handlers(httpd_handle_t server);
 esp_err_t start_server(void);
 
 
